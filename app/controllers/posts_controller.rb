2f719cec29bdae070da212b71306945b1a8ab75a
Mo Omer <beancinematics@gmail.com>
2014-01-01 12:15:59 -0600

    through 5.7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,20 @@
 class PostsController < ApplicationController
-	def new
-	end
+  def new
+  end
 
-	def create
-		render text: params[:post].inspect
-	end
+  def create
+    @post = Post.new(post_params)
+   
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
